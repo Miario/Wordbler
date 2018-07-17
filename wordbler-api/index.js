2bf6db1553a082159a357ad9a313fe4b7001bbb5
Miario <mario.indyushkin@gmail.com>
2018-07-17 10:37:48 -0700

    creating auth route and handler

--- wordbler-api/index.js
+++ wordbler-api/index.js
@@ -1,13 +1,17 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const errorHandler = require("./handlers/error");
+const authRoutes = require("./routes/auth");
+
 const PORT = 3001;
-const errorHandler = require("./handlers/error"); 
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api/auth", authRoutes);
 // all my routes here
 
 app.use(function(req, res, next) {
