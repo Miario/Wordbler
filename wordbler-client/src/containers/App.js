0c4b125ed3b5d768809329a0db4ed8be4a2be32d
Miario <mario.indyushkin@gmail.com>
2018-08-01 09:55:01 -0700

    adding Navbar

--- wordbler-client/src/containers/App.js
+++ wordbler-client/src/containers/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import {Provider} from "react-redux";
 import {configureStore} from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const store = configureStore()
 
 const App = () => (
   <Provider store={store}>
-    <div>Hello World</div>
+    <Router>
+      <div className="onboarding">
+        <Navbar />
+      </div>
+    </Router>
   </Provider>
 );
 
