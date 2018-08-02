ae04cf6262a792a228cb85609160d7ab39be6e70
Miario <mario.indyushkin@gmail.com>
2018-08-02 13:12:14 -0700

    adding jwt to http headers

--- wordbler-client/src/store/actions/auth.js
+++ wordbler-client/src/store/actions/auth.js
@@ -1,5 +1,5 @@
-import {apiCall} from "../../services/api";
-import {SET_CURRENT_USER} from "../actionTypes";
+import { apiCall, setTokenHeader } from "../../services/api";
+import { SET_CURRENT_USER } from "../actionTypes";
 import { addError, removeError } from "./errors";
 
 export function setCurrentUser(user) {
@@ -9,9 +9,14 @@ export function setCurrentUser(user) {
 	};
 }
 
+export function setAuthorizationToken(token) {
+	setTokenHeader(token);
+}
+
 export function logout() {
 	return dispatch => {
 		localStorage.clear();
+		setAuthorizationToken(false);
 		dispatch(setCurrentUser({}));
 	};
 };
@@ -23,6 +28,7 @@ export function authUser(type, userData) {
 			return apiCall("post", `/api/auth/${type}`, userData).then(
 				({ token, ...user }) => {
 					localStorage.setItem("jwtToken", token);
+					setAuthorizationToken(token);
 					dispatch(setCurrentUser(user));
 					dispatch(removeError());
 					resolve(); // Indicate that the api call was successful
