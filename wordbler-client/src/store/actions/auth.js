9c3969b065dc532615d757ba5abbebf4e699c89e
Miario <mario.indyushkin@gmail.com>
2018-08-02 10:26:12 -0700

    adding comments, cleaning up code

--- wordbler-client/src/store/actions/auth.js
+++ wordbler-client/src/store/actions/auth.js
@@ -10,14 +10,17 @@ export function setCurrentUser(user) {
 
 export function authUser(type, userData) {
 	return dispatch => {
+		// wrap our thunk in a promise and wait for the api call
 		return new Promise((resolve, reject) => {
 			return apiCall("post", `/api/auth/${type}`, userData).then(
 				({ token, ...user }) => {
 					localStorage.setItem("jwtToken", token);
 					dispatch(setCurrentUser(user));
-					resolve();
-				}
-			);
+					resolve(); // Indicate that the api call was successful
+				})
+			.catch (err => {
+				reject(); // Indicate that the api call was unsuccessful
+			});
 		});
 	};
 }
